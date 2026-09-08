e1138b5db8e3d89e4561083eaa79c5a7934828bc
Zara <sanchezsara9924@gmail.com>
2026-09-08 08:34:36 -0500

    mmm

--- model/detalle_recibo.php
+++ model/detalle_recibo.php
@@ -1,6 +1,6 @@
 <?php
 // model/detalle_recibo.php
-require_once __DIR__ . '/../config/conexion.php';
+require_once __DIR__ . '/../config/connection.php';
 
 class DetalleRecibo
 {
@@ -9,7 +9,7 @@ class DetalleRecibo
 
     public function __construct()
     {
-        $this->db = (new Conexion())->conn;
+        $this->db = (new Connection())->conn;
     }
 
     /**
